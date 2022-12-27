4b4982e292155715dd2ba24025062c9f12908738
yhann.carvalho <yhann.carvalho@castgroup.com.br>
2022-12-27 12:54:34 -0300

    Service and test to the Game

--- gamification/src/main/java/microservice/book/gamification/game/service/GameServiceImpl.java
+++ gamification/src/main/java/microservice/book/gamification/game/service/GameServiceImpl.java
@@ -7,20 +7,77 @@ import microservice.book.gamification.game.BadgeCardRepository;
 import microservice.book.gamification.game.GameService;
 import microservice.book.gamification.game.ScoreCardRepository;
 import microservice.book.gamification.game.badgeprocessors.BadgeProcessor;
+import microservice.book.gamification.game.domain.BadgeCard;
+import microservice.book.gamification.game.domain.BadgeType;
+import microservice.book.gamification.game.domain.ScoreCard;
 import org.springframework.stereotype.Service;
 
+import java.util.Collections;
 import java.util.List;
+import java.util.Optional;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 @Service
 @Slf4j
 @RequiredArgsConstructor
 public class GameServiceImpl implements GameService {
     private final ScoreCardRepository scoreRepository;
-    private final BadgeCardRepository badgeCardRepository;
+    private final BadgeCardRepository badgeRepository;
     private final List<BadgeProcessor> badgeProcessors;
 
     @Override
     public GameResult newAttemptForUser(final ChallengeSolvedDTO challenge) {
-        return null;
+        if (challenge.isCorrect()) {
+            ScoreCard scoreCard = new ScoreCard(challenge.getUserId(), challenge.getAttemptId());
+            scoreRepository.save(scoreCard);
+
+            log.info("User {} scored {} points for attempt id {}",
+                    challenge.getUserAlias(), scoreCard.getScore(), challenge.getAttemptId()
+            );
+
+            List<BadgeCard> badgeCards = processForBadges(challenge);
+            return new GameResult(
+                    scoreCard.getScore(),
+                    badgeCards.stream().map(BadgeCard::getBadgeType).collect(Collectors.toList())
+            );
+        } else {
+            log.info("Attempt id {} is not correct. " + "User {} does not get score.",
+                    challenge.getAttemptId(), challenge.getUserId()
+            );
+            return new GameResult(0, List.of());
+        }
+    }
+
+    /*
+    * Checks the total score and the different score cards obtained
+    * to give new badges in case their conditions are met.
+    */
+
+    private List<BadgeCard> processForBadges(final ChallengeSolvedDTO solvedChallenge) {
+        Optional<Integer> optTotalScore = scoreRepository.getTotalScoreForUser(solvedChallenge.getUserId());
+
+        if (optTotalScore.isEmpty())
+            return Collections.emptyList();
+
+        int totalScore = optTotalScore.get();
+
+        List<ScoreCard> scoreCardList = scoreRepository
+                .findByUserIdOrderScoreTimestampDesc(solvedChallenge.getUserId());
+
+        Set<BadgeType> alreadyGotBadges = badgeRepository
+                .findByUserIdOrderByBadgeTimestampDesc(solvedChallenge.getUserId())
+                .stream()
+                .map(BadgeCard::getBadgeType)
+                .collect(Collectors.toSet());
+
+        List<BadgeCard> newBadgeCards = badgeProcessors.stream()
+                .filter(bp -> !alreadyGotBadges.contains(bp.badgeType()))
+                .map(bp -> bp.processForOptionalBadge(totalScore, scoreCardList, solvedChallenge))
+                .flatMap(Optional::stream)
+                .map(badgeType -> new BadgeCard(solvedChallenge.getUserId(), badgeType)).toList();
+
+        badgeRepository.saveAll(newBadgeCards);
+        return newBadgeCards;
     }
 }
---
+++ gamification/src/test/java/microservice/book/gamification/game/GameServiceImplTest.java
@@ -0,0 +1,88 @@
+package microservice.book.gamification.game;
+
+import microservice.book.gamification.challenge.ChallengeSolvedDTO;
+import microservice.book.gamification.game.BadgeCardRepository;
+import microservice.book.gamification.game.GameService;
+import microservice.book.gamification.game.GameService.GameResult;
+import microservice.book.gamification.game.ScoreCardRepository;
+import microservice.book.gamification.game.badgeprocessors.BadgeProcessor;
+import microservice.book.gamification.game.domain.BadgeCard;
+import microservice.book.gamification.game.domain.BadgeType;
+import microservice.book.gamification.game.domain.ScoreCard;
+import microservice.book.gamification.game.service.GameServiceImpl;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.Set;
+
+import static org.assertj.core.api.BDDAssertions.then;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.verify;
+
+@ExtendWith(MockitoExtension.class)
+public class GameServiceImplTest {
+    private GameService gameService;
+
+    @Mock
+    private ScoreCardRepository scoreCardRepository;
+
+    @Mock
+    private BadgeCardRepository badgeCardRepository;
+
+    @Mock
+    private BadgeProcessor badgeProcessors;
+
+    @BeforeEach
+    void setUp() {
+        gameService = new GameServiceImpl(
+                scoreCardRepository,
+                badgeCardRepository,
+                List.of(badgeProcessors)
+        );
+    }
+
+    @Test
+    void processCorrectAttemptTest() {
+        long userId = 1L, attemptId = 10L;
+        var attempt =
+                new ChallengeSolvedDTO(attemptId, true, 20, 70, userId, "john_doe");
+        ScoreCard score =  new ScoreCard(userId, attemptId);
+
+        given(scoreCardRepository.getTotalScoreForUser(userId))
+                .willReturn(Optional.of(10));
+
+        given(scoreCardRepository.findByUserIdOrderScoreTimestampDesc(userId))
+                .willReturn(List.of(score));
+
+        given(badgeCardRepository.findByUserIdOrderByBadgeTimestampDesc(userId))
+                .willReturn(Set.of(new BadgeCard(userId, BadgeType.FIRST_WON)));
+
+        given(badgeProcessors.badgeType())
+                .willReturn(BadgeType.LUCKY_NUMBER);
+
+        given(badgeProcessors.processForOptionalBadge(10, List.of(score), attempt))
+                .willReturn(Optional.of(BadgeType.LUCKY_NUMBER));
+
+        final GameResult gameResult = gameService.newAttemptForUser(attempt);
+
+        then(gameResult)
+                .isEqualTo(new GameResult(10, List.of(BadgeType.LUCKY_NUMBER)));
+
+        verify(scoreCardRepository).save(score);
+        verify(badgeCardRepository).saveAll(List.of(new BadgeCard(userId, BadgeType.LUCKY_NUMBER)));
+    }
+
+    @Test
+    void processWrongAttemptTest() {
+        GameResult gameResult = gameService.newAttemptForUser(
+                new ChallengeSolvedDTO(10L, false, 10, 10, 1L, "john_doe")
+        );
+
+        then(gameResult).isEqualTo(new GameResult(0, List.of()));
+    }
+}
