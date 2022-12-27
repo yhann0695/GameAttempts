7c9941657be9c04106bea158d05398adc4a14fec
yhann.carvalho <yhann.carvalho@castgroup.com.br>
2022-12-27 12:53:39 -0300

    badgeTypes

--- gamification/src/main/java/microservice/book/gamification/game/domain/BadgeType.java
+++ gamification/src/main/java/microservice/book/gamification/game/domain/BadgeType.java
@@ -1,4 +1,17 @@
 package microservice.book.gamification.game.domain;
 
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
 public enum BadgeType {
+    BRONZE("Bronze"),
+    SILVER("Silver"),
+    GOLD("Gold"),
+
+    FIRST_WON("First time"),
+    LUCKY_NUMBER("Lucky number");
+
+    private final String description;
 }
