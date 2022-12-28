37cd9d11c0b2637f3185900150fc99fe0ceef70e
yhann.carvalho <yhann.carvalho@castgroup.com.br>
2022-12-28 00:56:54 -0300

    creating WebConfiguration and JsonConfiguration

---
+++ gamification/src/main/java/microservice/book/gamification/configutarion/WebConfiguration.java
@@ -0,0 +1,13 @@
+package microservice.book.gamification.configutarion;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfiguration implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(final CorsRegistry registry) {
+        registry.addMapping("/**").allowedOrigins("http://localhost:3000");
+    }
+}
